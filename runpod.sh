aaadeccccc6750e814d74487669eb9246f46ad0c
Melina Jingting Laimon <melinajingting@gmail.com>
2025-03-16 16:13:49 +0000

    minor fixes

--- runpod.sh
+++ runpod.sh
@@ -1,1 +1,1 @@
-scp  -P 1736 -i ~/.ssh/id_ed25519 data/sensorium_all_2023/*.zip root@34.197.182.78:sensorium/data/sensorium_all_2023/dynamic29515-10-12-Video-9b4f6a1a067fe51e15306b9628efea20.zip
+scp  -P 14510 -i ~/.ssh/id_ed25519 data/sensorium_all_2023/*.zip root@209.53.88.242:sensorium/data/sensorium_all_2023/dynamic29515-10-12-Video-9b4f6a1a067fe51e15306b9628efea20.zip
