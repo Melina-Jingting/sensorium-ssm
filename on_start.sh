b0ca0cb21e661b382933ff7edb841a13f1df80b1
Melina-Jingting <melinajingting@gmail.com>
2025-03-31 16:49:39 +0000

    Stop tracking on_start.sh

--- on_start.sh
+++
@@ -1,4 +0,0 @@
-source .venv/bin/activate
-git config --global user.email "melinajingting@gmail.com"
-git config --global user.name "Melina-Jingting"
-export PYTHONPATH="${PYTHONPATH}:/workspace/sensorium-ssm/"
